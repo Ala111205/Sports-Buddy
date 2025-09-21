fb54d8cd332bec5a5fe2dddf1bbc0c1d8f739af7
Ala111205 <sadham070403@gmail.com>
2025-09-21 13:36:11 +0530

    home

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,7 +2,7 @@ export default function Home(){
     return(
         <>
         <div className="home">
-            <div className="imageContainer" style={{backgroundImage: "url(/assests/Sport.png)", backgroundSize: "cover", backgroundPosition: "center"}}>
+            <div className="imageContainer" style={{backgroundImage: "url(/Sport.png)", backgroundSize: "cover", backgroundPosition: "center"}}>
                 <h2>“Playing sports builds not only physical strength but also mental resilience. It teaches teamwork, discipline, and perseverance while reducing stress and boosting confidence — helping you <span>grow stronger</span> both on and off the field.”</h2>
             </div>
         </div>
